3cc46fec3de9a9ba39ce110eda8163d3e05ccf1b
송서연 <sy.song@nextnow.kr>
2022-06-02 23:16:29 +0900

    06/02 스터디 #8

--- lib/src/pages/active_history.dart
+++ lib/src/pages/active_history.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:instargram_clone/src/components/avatar_widget.dart';
 
@@ -6,57 +5,46 @@ class ActiveHistory extends StatelessWidget {
   const ActiveHistory({Key? key}) : super(key: key);
 
   Widget _activeItemOne() {
-    return Row(
-      children: [
-        AvatarWidget(
-          type: AvatarType.TYPE2,
-          size: 40,
-          thumbPath: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTD-FPrClXiolofgRtHq_yf7p-K9XyZegxD3dqrYbMSba67vBVVighWoXZeRyOWqFl40FQ&usqp=CAU"
-        ),
-        const SizedBox(width: 10),
-        Expanded(
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10.0),
+      child: Row(
+        children: const [
+          AvatarWidget(
+            type: AvatarType.TYPE2,
+            size: 40,
+            thumbPath: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTD-FPrClXiolofgRtHq_yf7p-K9XyZegxD3dqrYbMSba67vBVVighWoXZeRyOWqFl40FQ&usqp=CAU"
+          ),
+          SizedBox(width: 10),
+          Expanded(
             child: Text.rich(
-                TextSpan(text: '여니포차님', style: TextStyle(fontWeight: FontWeight.bold)),
-
-            )
-        )
-      ],
+                TextSpan(
+                    text: '여니포차',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                    children: [
+                      TextSpan(text: '님이 회원님의 게시물을 좋아합니다.', style: TextStyle(fontWeight: FontWeight.normal)),
+                      TextSpan(text: ' 5 일전', style: TextStyle(fontWeight: FontWeight.normal, fontSize: 13, color: Colors.black54))
+                    ]
+                ),
+              )
+          )
+        ],
+      ),
     );
   }
 
-  Widget _newRecentlyActiveView() {
+  Widget _newRecentlyActiveView(String title) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 15.0),
+      padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 10.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          const Text('오늘', style: TextStyle(fontSize: 16)),
+          Text(title, style: const TextStyle(fontSize: 16)),
           const SizedBox(height: 15),
+          _activeItemOne(),
+          _activeItemOne(),
+          _activeItemOne(),
+          _activeItemOne(),
           _activeItemOne()
-        ],
-      ),
-    );
-  }
-
-  Widget _newRecentlyThisWeekView() {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 15.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: const [
-          Text('이번주', style: TextStyle(fontSize: 16))
-        ],
-      ),
-    );
-  }
-
-  Widget _newRecentlyMonthView() {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 15.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: const [
-          Text('이번달', style: TextStyle(fontSize: 16))
         ],
       ),
     );
@@ -74,9 +62,9 @@ class ActiveHistory extends StatelessWidget {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            _newRecentlyActiveView(),
-            _newRecentlyThisWeekView(),
-            _newRecentlyMonthView()
+            _newRecentlyActiveView('오늘'),
+            _newRecentlyActiveView('이번주'),
+            _newRecentlyActiveView('이번달')
           ],
         ),
       ),
